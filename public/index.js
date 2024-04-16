047030d97210d1713530f7ef9681ac8b4db6a1ae
Clue <108814268+C1u3@users.noreply.github.com>
2024-04-16 11:46:52 +0000

    Update

--- public/index.js
+++ public/index.js
@@ -5,8 +5,12 @@ const stand = document.getElementById("stand");
 const newGame = document.getElementById("game-btn");
 const betInput = document.getElementById("bet-input");
 const start = document.getElementById("start");
+const reshuffle = document.getElementById("reshuffle");
+const borrow = document.getElementById("borrow");
 const dealerHandContainer = document.getElementById("dealer-hand");
 const cardImagesContainer = document.getElementById("player-hand");
+
+borrow.style.display = "none";
 
 let deck = [...cardDeck];
 let betAmount;
@@ -18,6 +22,7 @@ let dragon = false;
 let dealerHand = [];
 let dealerPrevHand = [];
 let dealerDragon = false;
+let lastBorrow = 125;
 
 (() => {
   deck.forEach(card => {
@@ -30,8 +35,20 @@ let dealerDragon = false;
 })();
 
 // Remember to add async and await
-function wait(ms) {
+const wait = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const disableElements = (elements) => {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].disabled = true;
+  }
+}
+
+const enableElements = (elements) => {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].disabled = false;
+  }
 }
 
 const shuffleDeck = () => {
@@ -69,24 +86,22 @@ const calculateTotal = (arr) => {
 const blackjackCheck = () => {
   const total = calculateTotal(hand);
   if (hand.length === 5 && total === 21) {
+    hit.disabled = true;
     dragon = true;
-    log("YOU GOT A BLACKJACK DRAGON!");
+    log("YOU GOT A DRAGON BLACKJACK!");
   } else if (total === 21) {
-    log("You got 21!");
     hit.disabled = true;
+    log("You got blackjack!");
   } else if (total > 21) {
-    log("You busted.");
-    log("Why the fuck did you hit again?");
     hit.disabled = true;
+    log("You busted, your hand is higher than 21.");
     cardImagesContainer.style.background = "darkred";
   } else if (hand.length === 4) {
     log("You are 1 card away from a dragon.");
   } else if (hand.length === 5) {
+    hit.disabled = true;
     dragon = true;
     log("DRAGON!");
-  }
-  else {
-    log("You can choose to hit (draw a card) or stand (end turn).");
   }
 }
 
@@ -122,8 +137,7 @@ const renderPlayerCards = () => {
       flippedCards.push(card);
       cardContainer.classList.toggle("flip");
       if (flippedCards.length === hand.length) {
-        hit.disabled = false;
-        stand.disabled = false;
+        enableElements([hit, stand]);
         blackjackCheck();
       }
     };
@@ -164,19 +178,21 @@ const renderDealerCards = () => {
 }
 
 const log = (info) => {
+  const logEntries = gameLog.querySelectorAll('.log-entry');
+  logEntries.forEach(entry => entry.classList.remove('new-entry'));
+  
   const container = document.createElement("div");
   container.innerHTML = info
+  container.classList.add("log-entry", "new-entry");
   gameLog.appendChild(container);
+  
   gameLog.scrollTo(0, gameLog.scrollHeight);
 }
 
-log("Welcome to gambling!");
-log("I am your trustworthy emotional support bot.");
-log("99% of gamblers quit before they win just so you know.");
-log("Press new game and have fun!");
+log("Instructions: Your bank total is at the top of this text. To start playing, press new game. To draw a card, press hit. To end your turn, press stand. The objective of this game is to get 21 points or below, while having more points than the dealer's hand.");
 
 newGame.addEventListener("click", async () => {
-  newGame.disabled = true;
+  disableElements([newGame]);
   
   hand = [];
   prevHand = [];
@@ -190,61 +206,70 @@ newGame.addEventListener("click", async () => {
   dealerHandContainer.innerHTML = "";
   dealerHandContainer.style.background = "darkslategray";
   
-  log("You created a new game.");
   deck = [...cardDeck];
   shuffleDeck();
+  log("The dealer is shuffling the deck.");
+  
+  await wait(1000);
+  log("Deck shuffled.");
 
   await wait(1000);
-  log("The dealer shuffled the deck.");
-
-  await wait(1000);
-  betInput.disabled = false;
+  enableElements([betInput, start, reshuffle]);
   log("Please input a bet amount ($).");
-
-  start.disabled = false;
 });
 
 start.addEventListener("click", async () => {
-  hit.disabled = true;
-  stand.disabled = true;
+  disableElements([hit, stand, reshuffle]);
   
   betAmount = parseInt(betInput.value);
   if (!betAmount) {
-    log("You have autism?")
     log("Please key in a valid amount.");
     return;
   } else if (betAmount > bank) {
     log("You can't afford to bet that amount.");
     return;
   }
-
-  start.disabled = true;
-  betInput.disabled = true;
   
-  log(`You placed a bet of $${betAmount}.`);
+  disableElements([start, betInput]);
 
-  await wait(1000);
+  if (betAmount === bank) {
+    log(`You went all in at a bet of $${betAmount}`);
+  } else {
+    log(`You placed a bet of $${betAmount}.`);
+  }
+
+  await wait(500);
   dealerHand.push(drawCard());
   renderDealerCards();
 
-  await wait(1000);
+  await wait(500);
   hand.push(drawCard());
   renderPlayerCards();
 
-  await wait(1000);
+  await wait(500);
   dealerHand.push(drawCard());
   renderDealerCards();
 
-  await wait(1000);
+  await wait(500);
   hand.push(drawCard());
   renderPlayerCards();
 
   log("Click on your cards to reveal them.");
+});
+
+reshuffle.addEventListener("click", async () => {
+  disableElements([reshuffle, betInput, start]);
+  
+  shuffleDeck();
+  log("The dealer is reshuffling the deck.");
+
+  await wait(2000);
+  enableElements([betInput, start]);
+  log("Deck reshuffled.");
 })
 
 hit.addEventListener("click", () => {
-  hit.disabled = true;
-  stand.disabled = true;
+  disableElements([hit, stand]);
   
   hand.push(drawCard());
   renderPlayerCards();
@@ -252,42 +277,47 @@ hit.addEventListener("click", () => {
 });
 
 stand.addEventListener("click", async () => {
-  hit.disabled = true;
-  stand.disabled = true;
-  log("You stand.");
+  disableElements([hit, stand]);
 
   const total = calculateTotal(hand);
-  
-  await wait(1000);
   log(`You have ${total} points.`);
 
   let dealerHandTotal = calculateTotal(dealerHand);
+
+  await wait(1000);
+  log("It is the dealer's turn.");
   
   while (dealerHandTotal <= 16 && dealerHand.length <= 4) {
     dealerHand.push(drawCard());
     dealerHandTotal = calculateTotal(dealerHand);
-    if (dealerHand.length === 5 && dealerHandTotal <= 21) {
-      dealerDragon = true;
-      log("Dealer got a dragon! (Dev note: I have yet to complete the winning condition for this yet, report to me and refresh if you see this pls.");
-    }
-    await wait(1000);
+    await wait(2000);
     renderDealerCards();
     log("The dealer decided to hit.");
   }
 
   await wait(2000);
+  
   dealerHandContainer.childNodes.forEach(child => {
       if (child.nodeType === 1) {
           child.classList.toggle('flip');
       }
   });
-  log(`Dealer has ${dealerHandTotal} points.`);
+  await wait(500);
+
+  if (dealerHand.length === 5 && dealerHandTotal <= 21) {
+    dealerDragon = true;
+    log("Dealer got a dragon! (Dev note: I have yet to complete the winning condition for this yet, report to me and refresh if you see this pls.");
+  } else {
+    log(`Dealer has ${dealerHandTotal} points.`);
+  }
 
   if (dealerHandTotal > 21) {
     dealerHandContainer.style.background = "darkred";
     log("Dealer busted.");
   }
 
+  await wait(2000);
+  
   if (dragon) {
     log(`You got a dragon! You won $${betAmount * 2}(2x)!`);
     bank += betAmount * 2;
@@ -295,27 +325,23 @@ stand.addEventListener("click", async () => {
     log(`You got a blackjack dragon! You won $${betAmount * 3}(3x)!`);
     bank += betAmount * 3;
   } else if (total > 21 && dealerHandTotal > 21) {
-    log("Both you and dealer busted.");
-    log("Both also missing a chromosome.")
+    log("Both you and the dealer busted.");
   } else if (total === dealerHandTotal) {
-    log("You tied with dealer, nigger.");
+    log("You tied with the dealer.");
   } else if (total === 21 && dealerHandTotal !== total) {
     log(`You won $${betAmount * 2}(2x)!`);
     bank += betAmount * 2;
   } else if (dealerHandTotal === 21 && total !== dealerHandTotal) {
     log(`You lost $${betAmount * 2}(2x).`);
-    log("LOL.");
     bank -= betAmount * 2;
   } else if (total > dealerHandTotal && total < 21) {
     log(`You won $${betAmount}!`);
     bank += betAmount;
   } else if (dealerHandTotal > total && dealerHandTotal < 21) {
     log(`You lost $${betAmount}.`);
-    log("Kill yourself.");
     bank -= betAmount;
   } else if (total > 21 && dealerHandTotal < 21) {
     log(`You lost $${betAmount}.`);
-    log("You fucking retarded.")
     bank -= betAmount;
   } else if (dealerHandTotal > 21 && total < 21) {
     log(`You won $${betAmount}!`);
@@ -326,10 +352,26 @@ stand.addEventListener("click", async () => {
 
   bankText.innerHTML = `Bank: $${bank}`;
 
-  if (bank <= 1 ) {
+  if (bank <= 0 ) {
+    await wait(3000);
     log("You are now bankrupt and homeless. You should go and borrow some money.");
+    borrow.style.display = "block";
     return;
   }
 
-  newGame.disabled = false;
+  enableElements([newGame]);
 });
+
+borrow.addEventListener("click", async () => {
+  borrow.style.display = "none";
+  log("You decided it was a good idea to borrow some money from a loan shark.");
+
+  const borrowAmount = lastBorrow * 2;
+  lastBorrow = borrowAmount;
+  bank += borrowAmount;
+  await wait(4000);
+  bankText.innerHTML = `Bank: $${bank}`;
+  log(`You borrowed $${borrowAmount}.`);
+
+  enableElements([newGame]);
+})
